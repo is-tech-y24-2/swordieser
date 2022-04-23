51d8e250f42a0d0c80e630ede8506a4ee83880e8
Sergei Ivanov <swordie.ser@gmail.com>
2022-04-23 14:08:47 +0300

    feat: added id to returning statements

--- kotiki-java/web/src/main/java/ru/itmo/kotiki/webModel/CatDto.java
+++ kotiki-java/web/src/main/java/ru/itmo/kotiki/webModel/CatDto.java
@@ -9,12 +9,14 @@ import java.sql.Date;
 
 
 public class CatDto {
+    private final int id;
     private final String name;
     private final Date birthday;
     private final CatBreed breed;
     private final CatColor color;
 
-    public CatDto(String name, Date birthday, CatBreed breed, CatColor color) {
+    public CatDto(int id, String name, Date birthday, CatBreed breed, CatColor color) {
+        this.id = id;
         this.name = name;
         this.birthday = birthday;
         this.breed = breed;
@@ -22,10 +24,15 @@ public class CatDto {
     }
 
     public CatDto(Cat cat) {
+        this.id = cat.getId();
         this.name = cat.getName();
         this.birthday = cat.getBirthday();
         this.breed = cat.getBreed();
         this.color = cat.getColor();
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
--- kotiki-java/web/src/main/java/ru/itmo/kotiki/webModel/OwnerDto.java
+++ kotiki-java/web/src/main/java/ru/itmo/kotiki/webModel/OwnerDto.java
@@ -7,17 +7,24 @@ import java.sql.Date;
 
 
 public class OwnerDto {
+    private final int id;
     private final String name;
     private final Date birthday;
 
-    public OwnerDto(String name, Date birthday) {
+    public OwnerDto(int id, String name, Date birthday) {
+        this.id = id;
         this.name = name;
         this.birthday = birthday;
     }
 
     public OwnerDto(Owner owner) {
+        this.id = owner.getId();
         this.name = owner.getName();
         this.birthday = owner.getBirthday();
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
